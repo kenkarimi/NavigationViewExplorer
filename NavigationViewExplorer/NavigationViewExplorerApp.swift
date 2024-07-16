6e1ae2587581bb6a567aea040179ce78c56741e9
Kennedy Karimi on Mac <kennedymugera@gmail.com>
2024-07-16 19:55:33 +0300

    Initial Commit

---
+++ NavigationViewExplorer/NavigationViewExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewExplorerApp.swift
+//  NavigationViewExplorer
+//
+//  Created by Kennedy Karimi on 16/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
